a125a1c1ace531fb2311cd46a798161121932e6b
chenfeifan12 <chenfeifan12@users.noreply.github.com>
2026-06-27 00:57:23 +0000

    feat: 开发多语种在线教育平台
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function AppLayout({ children }: { children: React.ReactNode }) {
       <Header />
       <div className="flex">
         <Sidebar />
-        <main className="flex-1 lg:ml-64">
+        <main className="flex-1">
           {children}
         </main>
       </div>
